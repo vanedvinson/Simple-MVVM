c12d678a1191c0da8b67b03bbb4143a246d5a0f2
Edvin Kovacevic <ekovacevic.10@gmail.com>
2021-11-05 00:44:52 +0100

    created some files

---
+++ simplemvvm/Client/ViewModels/FetchDataViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleMVVM.Client.ViewModels
+{
+    public class FetchDataViewModel
+    {
+    }
+}
