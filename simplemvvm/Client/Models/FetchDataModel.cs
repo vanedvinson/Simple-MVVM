1425662b2282da4305ace27becbc202984387690
Edvin Kovacevic <ekovacevic.10@gmail.com>
2021-11-08 12:45:37 +0100

    Edited model

--- simplemvvm/Client/Models/FetchDataModel.cs
+++ simplemvvm/Client/Models/FetchDataModel.cs
@@ -1,11 +1,30 @@
-﻿ using System;
+﻿using SimpleMVVM.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
+
 
 namespace SimpleMVVM.Client.Models
 {
-    public class FetchDataModel
+    public interface IFetchDataModel
     {
+        Task<WeatherForecast[]> RetrieveForecastsAsync();
+        
+    }
+    public class FetchDataModel : IFetchDataModel
+    {
+        private HttpClient _http;
+        public FetchDataModel(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<WeatherForecast[]> RetrieveForecastsAsync()
+        {
+            return await _http.GetFromJsonAsync<WeatherForecast[]>("api/SampleData/WeatherForecasts");
+        }
     }
 }
