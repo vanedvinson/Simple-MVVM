34213ffcece9acc1028f27aaa60038ae254fc2a3
Edvin Kovacevic <ekovacevic.10@gmail.com>
2021-11-08 12:56:49 +0100

    updates

--- simplemvvm/Client/ViewModels/FetchDataViewModel.cs
+++ simplemvvm/Client/ViewModels/FetchDataViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SimpleMVVM.Client.Models;
 using SimpleMVVM.Shared;
 
 namespace SimpleMVVM.Client.ViewModels
@@ -10,20 +11,29 @@ namespace SimpleMVVM.Client.ViewModels
     {
         //We have made a private array with a public property in the class to hold and expose our WeatherForecast[].
         WeatherForecast[] WeatherForecasts { get; set; }
+        Task RetrieveForecastsAsync();
     }
     public class FetchDataViewModel : IFetchDataViewModel
     {
         private WeatherForecast[] _weatherForecasts;
-        private IFetchDataViewModel _fetchDataViewModel;
+        private IFetchDataModel _fetchDataModel;
         public WeatherForecast[] WeatherForecasts
         {
             get => _weatherForecasts;
             set => _weatherForecasts = value;
         }
-        public FetchDataViewModel(IFetchDataViewModel fetchDataViewModel)
+        public FetchDataViewModel(IFetchDataModel fetchDataModel)
         {
             Console.WriteLine("FetchDataViewModel Constructor Executing");
-            _fetchDataViewModel = fetchDataViewModel;
+            _fetchDataModel = fetchDataModel;
         }
+
+        public async Task RetrieveForecastsAsync()
+        {
+            _weatherForecasts = await _fetchDataModel.RetrieveForecastsAsync();
+            Console.WriteLine("FetchDataViewModel Forecasts Retrieved");
+        }
+
+   
     }
 }
